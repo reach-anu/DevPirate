59bfbd1e2a73782f6a5278239ea7d212dc2e924a
anjali-8001 <anjalisharma8001@gmail.com>
2024-02-19 17:42:01 +0530

    create wave animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
     <div>
-      Hello
-      <Footer/>
+      <Home />
+      <Footer />
     </div>
   );
 }
