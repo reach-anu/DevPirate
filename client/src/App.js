a1ea931958bd460739ddf5c6ef4784ffe1d2b53a
Devansh Sahni <devanshsahni@gmail.com>
2024-03-03 12:41:57 +0000

    Create login and signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import Navbar from "./Components/Navbar";
 import LandingPage from "./Pages/LandingPage";
 import Footer from "./Components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
     <div>
-    <Navbar />
-    <LandingPage />
-    <Footer />
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_ID}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Footer />
+      </GoogleOAuthProvider>
     </div>
   );
 }
