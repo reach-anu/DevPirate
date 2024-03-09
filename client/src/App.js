2a62e48a3ff78b555eb8a99eeec19a96685006d2
anjali-8001 <anjalisharma8001@gmail.com>
2024-03-09 21:59:19 +0530

    Create offerings landing page

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,24 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Signup from "./Pages/Signup";
+import OfferingsPage1 from "./Pages/OfferingsPage1";
+import OfferingsPage2 from "./Pages/OffieringsPage2";
 
 function App() {
   return (
+    // <>
+    //   <section>
+    //     <Navbar />
+    //     <Home />
+    //   </section>
+    //   <section>
+    //     <OfferingsPage1 />
+    //   </section>
+    //   <section>
+    //     <OfferingsPage2 />
+    //     <Footer />
+    //   </section>
+    // </>
     <div>
       <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_ID}>
         <Navbar />
