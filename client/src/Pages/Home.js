3d0759e7dfe393943a79b56a04aa70549bac7190
anjali-8001 <anjalisharma8001@gmail.com>
2024-03-10 08:29:35 +0530

    create offerings landing page

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import LandingPage from "../Components/Home/LandingPage";
+import OfferingsPage1 from "../Components/Home/OfferingsPage1";
+import OfferingsPage2 from "../Components/Home/OffieringsPage2";
+import Footer from "../Components/Footer";
+
+function Home() {
+  return (
+    <>
+      <section className="snap-start">
+        <LandingPage />
+      </section>
+      <section className="snap-center">
+        <OfferingsPage1 />
+      </section>
+      <section className="snap-center">
+        <OfferingsPage2 />
+        <Footer />
+      </section>
+    </>
+  );
+}
+
+export default Home;
